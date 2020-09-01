c08b4ca320574ad8ac3af493e71cd72623ec286a
Matthew Mccarthy <matthewmc96@gmail.com>
2020-08-31 22:53:29 -0500

    done for sure this time...I think

--- media-queries-afternoon1/src/components/Header.js
+++ media-queries-afternoon1/src/components/Header.js
@@ -1,25 +1,42 @@
 import React, { Component } from "react";
 
-
-
 export default class Header extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      display: false,
+    };
+
+    this.handleSlide = this.handleSlide.bind(this);
+  }
+
+  handleSlide() {
+    this.setState({
+      display: !this.state.display,
+    });
+  }
 
   render() {
- 
     return (
-      
+      <div>
         <div className="navbar">
           <h1 className="app-name">NavBar</h1>
-
-          
+          <div className="navbar-icon" onClick={this.handleSlide}>
+            &#9776;
+          </div>
           <div className="navbar-button-container">
+            <button className="navbar-button">Home</button>
             <button className="navbar-button">About</button>
-            <button className="navbar-button">Projects</button>
             <button className="navbar-button">Contact</button>
           </div>
+          <div className={this.state.display ? "dropdown" : "hidden"}>
+            <button className="navbar-button">Home</button>
+            <button className="navbar-button">About</button>
+            <button className="navbar-button">Contact</button>
           </div>
-    
+        </div>
+      </div>
     );
   }
 }
